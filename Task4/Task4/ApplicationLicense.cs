3eb3ed752a493285d3ae4e5c5d8ab389b56c24dc
DmitryKhvostenko <112482754+DmitryKhvostenko@users.noreply.github.com>
2022-10-07 18:32:30 +0300

    Add files via upload

---
+++ Task4/Task4/ApplicationLicense.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class ApplicationLicense
+    {
+        public bool Common { get; set; }
+        public bool Trial { get; set; }
+        public bool Pro { get; set; }
+
+        public string TrialKey { get; set; }
+        public string ProKey { get; set; }
+
+        public ApplicationLicense(string key)
+        {
+            this.TrialKey = "Trial";
+            this.ProKey = "Pro";
+
+            if (key == "Trial") { this.AllowTrial(); }
+            else if (key == "Pro") { this.AllowPro(); }
+            else {this.AllowCommon(); }
+        }
+
+        public void AllowCommon()
+        {
+            this.Common = true;
+            Console.WriteLine("Неверный ключ. Ваша лицензия: 'Стандартная'!");
+        }
+
+        public void AllowTrial()
+        {
+            this.Trial = true;
+            this.Common = false;
+            Console.WriteLine("Ваша лицензия: 'Пробный период'");
+        }
+
+        public void AllowPro()
+        {
+            this.Pro = true;
+            this.Common = false;
+            Console.WriteLine("Ваша лицензия: 'Продвинутая'");
+        }
+
+    }
+}
---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Введите ключ: ");
+
+                string msg = Console.ReadLine();
+                ApplicationLicense applicationLicense = new ApplicationLicense(msg);
+
+                break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
